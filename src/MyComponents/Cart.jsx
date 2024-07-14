a2e51cb4a54d3cecbd2ba374ec39724f35f42ba3
Bhavyadoshi266 <doshibhavy0712@gmail.com>
2024-07-14 13:46:32 +0530

    change in css of cart.jsx

--- src/MyComponents/Cart.jsx
+++ src/MyComponents/Cart.jsx
@@ -25,7 +25,7 @@ export default function Cart() {
 
     <div>
       <div className="common">
-        <div className="wrapper">
+        <div className="wrapper" style={{maxWidth:"900px"}}>
           <p className='headerfor_o_o pb-5'>Order List</p>
           <div className="cartfor_o_o_continer">
             {cart.length > 0 ? (
@@ -34,17 +34,17 @@ export default function Cart() {
                   {cart.map((cartItem) => (
                     <div className="container" key={cartItem.id}>
                       <div className="cartfor_o_o  row">
-                        <div className="col-9 pt-4">
+                        <div className="col-11 pt-4">
                           <p className="m-0" style={{ fontWeight: '600' }}>
                             {cartItem.dishname}
                           </p>
                         </div>
 
-                        <div className="col-3 pt-4 ">
+                        <div className="col-1 pt-4 ">
                           <img className="delete" src={trash} alt="" height='24px' style={{ cursor: 'pointer' }} onClick={() => onDelete(cartItem.id)} />
                         </div>
 
-                        <div className="col-9 py-3 pb-2 px-0">
+                        <div className="col-11 py-3 pb-2 px-0">
                           <p className='my-0' style={{ fontWeight: '500', color: '#311f09' }}>{cartItem.price}</p>
                         </div>
 
@@ -60,42 +60,43 @@ export default function Cart() {
                         <div className="col-12 py-4 pb-3" style={{ borderTop: '1px dashed rgb(209 204 204)', width: '100%' }}>
                           <p className="m-0" style={{ fontWeight: '600' }}>Voucher Code</p>
                         </div>
-                        <div className="col-9 py-4 pt-0">
+                        <div className="col-10 py-4 pt-0">
                           <p className='coupon_code m-0' style={{backgroundColor:"#53a5e0",color:"#ffffff"}}>FREETOEAT</p>
                         </div>
-                        <div className="col-3 px-0 py-4 pt-0" onClick={() => applyVoucher()} style={{ cursor: 'pointer' }}>
+                        <div className="col-2 px-0 py-4 pt-0" onClick={() => applyVoucher()} style={{ cursor: 'pointer',textAlign:"end",}}>
                           <i className="add fa-solid fa-plus"></i>
                         </div>
                         <div className="col-12 py-3" style={{ borderTop: '1px dashed rgb(209 204 204)', width: '100%' }}>
                         </div>
-                        <div className="col-9 py-2 ">
+                        <div className="col-11 py-2 ">
                           <p className="m-0" style={{ fontWeight: '600' }}>Subtotal</p>
                         </div>
-                        <div className="col-3 p-0" style={{ fontWeight: '500', color: '#311f09' }}>
+                        <div className="col-1 p-0" style={{ fontWeight: '500', color: '#311f09' }}>
                           $78.30
                         </div>
-                        <div className="col-9 py-2" >
+                        <div className="col-11 py-2" >
                           <p className="m-0" style={{ fontWeight: '600' }}>Tax fee</p>
                         </div>
-                        <div className="col-3 p-0" style={{ fontWeight: '500', color: '#311f09' }}>
+                        <div className="col-1 p-0" style={{ fontWeight: '500', color: '#311f09' }}>
                           $3.50
                         </div>
-                        <div className="col-9 py-2">
+                        <div className="col-11 py-2">
                           <p className="m-0" style={{ fontWeight: '600' }}>Voucher</p>
                         </div>
-                        <div className="col-3 p-0" style={{ fontWeight: '500', color: '#311f09' }}>
+                        <div className="col-1 p-0" style={{ fontWeight: '500', color: '#311f09' }}>
                           {voucherApplied ? "-$5.00" : ""}
                         </div>
-                        <div className="col-9 py-2">
+                        <div className="col-11 py-2">
                           <p className="m-0" style={{ fontWeight: '600' }}>Total</p>
                         </div>
-                        <div className="col-3 p-0" style={{ fontWeight: '500', color: '#311f09' }}>
+                        <div className="col-1 p-0" style={{ fontWeight: '500', color: '#311f09' }}>
                           $76.8
                         </div>
 
-                        <div className="col-10 px-0 pt-5 pb-0">
+                        <div className="col-12 px-0 pt-5 pb-0">
                           <Link to="/checkout"> <button className="btn_checkout">Checkout</button></Link>
                         </div>
+
                       </div>
                     </div>
                   )}
